7c4db9953ad13184bbb2fba34a21d6d504b9d20d
altafsayeed <altafsayeed19@yahoo.com>
2022-10-12 02:40:30 -0700

    all CRUD functions working

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
 
   return (
     <Container maxwidth="lg">
@@ -33,6 +33,7 @@ const App = () => {
       <Grow in>
         <Container>
           <Grid
+            className={classes.mainContainer}
             container
             justifyContent="space-between"
             alignItems="stretch"
